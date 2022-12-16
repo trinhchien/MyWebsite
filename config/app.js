6f945ac9a1a5dc66d977950000c05ed9d6cb3f89
trinhchien <trinhdacchien1598@gmail.com>
2022-12-16 22:44:24 +0700

    Revert "install express session"
    This reverts commit 39105ec48c4b696e50b3275fe3ddb9313c2d3199.

--- config/app.js
+++ config/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import * as dotenv from 'dotenv';
-import session from 'express-session';
 dotenv.config({ path: './config/.env' });
 
 const app = express();
@@ -10,14 +9,6 @@ const app = express();
 app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-    session({
-        secret: process.env.SECRET_SESSION,
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: true },
-    })
-);
 
 const port = process.env.PORT || 3000;
 
